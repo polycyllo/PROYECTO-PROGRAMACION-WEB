f786f91b42c99044800dcf792d12cf65e0a6aba3
Isac Poly Bolivar Puma <119465061+polycyllo@users.noreply.github.com>
2024-12-12 23:55:45 -0400

    cambio inf

--- src/data/bd.ts
+++ src/data/bd.ts
@@ -3,24 +3,25 @@ export const preguntaRespuesta: PreguntaRespuesta[] = [
     {
         id: 1,
         pregunta: "¿Qué es ezzForm?",
-        respuesta: "Es una plataforma para crear formularios",
+        respuesta:
+            "ezzForm es una plataforma en línea diseñada para facilitar la creación de formularios personalizados para recopilar datos de manera eficiente, ya sea para encuestas, registros, feedback o cualquier tipo de recolección de información.",
     },
     {
         id: 2,
         pregunta: "¿Por qué usar ezzForm?",
         respuesta:
-            "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempore ut tempora doloribus minima, eligendi consequatur fuga nesciunt cumque culpa incidunt eos architecto minus omnis, beatae itaque unde. Perferendis, sapiente nostrum",
+            "Usar ezzForm permite crear formularios fácilmente sin necesidad de conocimientos técnicos. Su interfaz amigable, junto con plantillas personalizables, ahorra tiempo y simplifica el proceso de creación de formularios, mientras se asegura una experiencia de usuario fluida y sin complicaciones.",
     },
     {
         id: 3,
         pregunta: "¿Quienes pueden usarlo?",
         respuesta:
-            "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempore ut tempora doloribus minima, eligendi consequatur fuga nesciunt cumque culpa incidunt eos architecto minus omnis, beatae itaque unde. Perferendis, sapiente nostrum",
+            "ezzForm está diseñado para ser utilizado por cualquier persona que necesite crear formularios, desde pequeñas empresas, educadores, hasta grandes corporaciones.",
     },
     {
         id: 4,
         pregunta: "¿Cómo empezar?",
         respuesta:
-            "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempore ut tempora doloribus minima, eligendi consequatur fuga nesciunt cumque culpa incidunt eos architecto minus omnis, beatae itaque unde. Perferendis, sapiente nostrum",
+            "Para comenzar con ezzForm, solo necesitas crear una cuenta en la plataforma, iniciar sesión y empezar a diseñar tus formularios",
     },
 ];
